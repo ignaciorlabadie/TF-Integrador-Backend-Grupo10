980b4a55e5f9cb44951f517df9fab84fa9ff38fd
Ignacio <ignacioramirezlabadie@gmail.com>
2026-09-23 16:05:37 -0300

    chore: configurar husky y pre-commit hooks

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,9 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  {
-    ignores: ['dist', 'node_modules'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+    {
+        ignores: ['dist', 'node_modules'],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
 )
---
+++ tests/TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+## Testing
+
+- [ ] Revisar `scripts.test` en `package.json`.
+- [ ] Quitar `--passWithNoTests` cuando existan tests reales.
